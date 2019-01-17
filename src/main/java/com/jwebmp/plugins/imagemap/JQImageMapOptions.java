6d90164a2bcdb051a7afe673fee2eccd7d656478
GedMarc <ged_marc@hotmail.com>
2019-01-17 22:00:40 +0200

    GPG Update

--- src/main/java/com/jwebmp/plugins/imagemap/JQImageMapOptions.java
+++ src/main/java/com/jwebmp/plugins/imagemap/JQImageMapOptions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Marc Magon
+ * Copyright (C) 2017 GedMarc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@ package com.jwebmp.plugins.imagemap;
 import com.jwebmp.core.htmlbuilder.javascript.JavaScriptPart;
 
 /**
- * @author Marc Magon
+ * @author GedMarc
  * @version 1.0
  * @since 30 Aug 2015
  */
